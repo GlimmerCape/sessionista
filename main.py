c94422512e1990aaae0e7f71122a0feb0331612e
Kamelov Nurlybek <nurlybek.kamelov@netcracker.com>
2024-10-09 19:05:19 +0500

    add arg to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import argparse
+
 import datetime
 
 import pathionista
@@ -5,8 +7,25 @@ import parsinista
 
 
 def main():
+    parser = argparse.ArgumentParser(description="CLI that remembers the last username and optionally takes output file path.")
+    
+    parser.add_argument(
+        "-p","--profile", 
+        type=bool, 
+        help="The username of the user. If not provided, the last used username will be reused."
+    )
+    parser.add_argument(
+        "-o","--output_file_path", 
+        type=str,
+        default=None,
+        help="Output file path. Defaults to timestamp + postfix."
+    )
     session_file = pathionista.get_session_file()
-    session_data = parsinista.convert_and_save_session(session_file, generate_filename())
+    args = parser.parse_args()
+    file_path = args.output_file_path
+    if file_path == None:
+        file_path = generate_filename()
+    session_data = parsinista.convert_and_save_session(session_file, file_path)
 
 
 def generate_filename(postfix="firefox_session", extension="json"):
