8b2f31df29460c1f951138d96641cd87633e918a
Kamelov Nurlybek <nurlybek.kamelov@netcracker.com>
2024-11-15 10:59:11 +0500

    change style of messages given by clinista get_user_choice()

--- getinista/clinista.py
+++ getinista/clinista.py
@@ -3,19 +3,21 @@ def get_user_choice(options: list[str], prompt: str) -> int | None:
         print(f"{prompt}. No options available to choose from")
         return None
 
+    print(f"\n{prompt}")
     for i, option in enumerate(options, start=1):
         print(f"{i}: {option}")
 
     while True:
         try:
-            choice = int(input(f"{prompt} (Enter the number or 0 to cancel): "))
+            choice = int(input(f"Enter the number or 0 to cancel: "))
             if choice == 0:
                 print("Operation cancelled by the user.")
                 return None
             if 1 <= choice <= len(options):
                 return choice - 1
             else:
-                print(f"Please enter a number between 1 and {len(options)}, or 0 to cancel.")
+                print(
+                    f"Please enter a number between 1 and {len(options)}, or 0 to cancel."
+                )
         except ValueError:
             print("Invalid input. Please enter a valid number.")
-
