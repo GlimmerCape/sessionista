518f44b21e2f00a06db7f0e27e13ae461836b95d
Kamelov Nurlybek <nurlybek.kamelov@netcracker.com>
2024-11-19 13:56:31 +0500

    feat(getinista): pre/appending of wildcards to patterns unless --raw-pattern

--- getinista/main.py
+++ getinista/main.py
@@ -4,6 +4,7 @@ import datetime
 import pathionista
 from parsinista import Parsinista
 
+PYTHON_WILDCARD = ".*"
 
 def main():
     parser = argparse.ArgumentParser(description="CLI tool for getting and saving firefox session files.")
@@ -21,19 +22,30 @@ def main():
         help="Session files will be filtered by this pattern using python regex",
     )
     parser.add_argument(
+        "-r", "--raw-patterns",
+        type=bool,
+        default=False,
+        help="By default cli adds '.*' at the beginning and end of the patterns"
+    )
+    parser.add_argument(
         "-o","--output-file-path", 
         type=str,
         default=None,
         help="Output file path. Defaults to timestamp + postfix."
     )
     args = parser.parse_args()
-    session_file_path = pathionista.get_session_file(profile_pattern=args.profile_pattern, session_pattern=args.session_pattern)
+    profile_pattern = args.profile_pattern if args.raw_patterns else add_wildcards_to_pattern(args.profile_pattern)
+    session_pattern = args.session_pattern if args.raw_patterns else add_wildcards_to_pattern(args.session_pattern)
+
+    session_file_path = pathionista.get_session_file(profile_pattern=profile_pattern, session_pattern=session_pattern)
     if not session_file_path:
         print("No session file selected")
         return
+
     output_file_path = args.output_file_path
     if output_file_path == None:
         output_file_path = generate_filename()
+
     parser = Parsinista(session_file_path, output_file_path)
     parser.convert_and_save_session()
 
@@ -45,6 +57,8 @@ def generate_filename(postfix="firefox_session", extension="json"):
     filename = f"{timestamp}_{postfix}.{extension}"
     return filename
 
+def add_wildcards_to_pattern(pattern: str) -> str:
+    return PYTHON_WILDCARD + pattern + PYTHON_WILDCARD    
 
 if __name__ == "__main__":
     main()
